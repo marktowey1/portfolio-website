56ce7be1c4a3bbb70f0c95f4b369613d43282ca2
Mark Towey <marktowey1@gmail.com>
2024-07-22 10:30:52 +0100

    Added header and About components

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div>
+      <div className="about-top">
+        <div className="about-info"></div>
+        <div className="about-photo"></div>
+      </div>
+      <div className="about-bottom">
+        <div className="social-links">
+          <a href="mailto:marktowey1@gmail.com">Contact</a>
+          <a href="https://www.linkedin.com/in/mark-towey-88231914a/">
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Mark Towey</h1>
+      <div className="header-menu">
+        <h2>Projects</h2>
+        <h2>Skills</h2>
+        <h2>Work</h2>
+        <h2>Contact</h2>
+        <h2>Dark/Light</h2>
+      </div>
+    </div>
+  );
+}
