9d585a6f2a8a389e237228d02185deef2d9cdb60
Mark Towey <marktowey1@gmail.com>
2024-07-23 15:39:17 +0100

    Added images and styling to Header and About components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,36 @@
+import "./About.css";
+import LinkedInIcon from "../images/Linkedin-icon.png";
+import TwitterIcon from "../images/Twitter-icon.png";
+import GithubIcon from "../images/Github-icon.png";
+import GmailIcon from "../images/Gmail-icon.png";
+import WebDevImage from "../images/WebDev-image.jpg";
+
 export default function About() {
   return (
     <div>
       <div className="about-top">
-        <div className="about-info"></div>
-        <div className="about-photo"></div>
+        <div className="about-info">
+          <p>
+            Hello, my name is <b>Mark Towey</b>. I am an expert at ReactJS.
+          </p>
+        </div>
+        <div className="about-photo">
+          <img src={WebDevImage} alt="Web Development Image" />
+        </div>
       </div>
       <div className="about-bottom">
         <div className="social-links">
-          <a href="mailto:marktowey1@gmail.com">Contact</a>
+          <a href="mailto:marktowey1@gmail.com">
+            <img className="socials-icon" src={GmailIcon}></img>
+          </a>
           <a href="https://www.linkedin.com/in/mark-towey-88231914a/">
-            LinkedIn
+            <img className="socials-icon" src={LinkedInIcon}></img>
+          </a>
+          <a href="https://x.com/toweym3">
+            <img className="socials-icon" src={TwitterIcon}></img>
+          </a>
+          <a href="https://github.com/marktowey1">
+            <img className="socials-icon" src={GithubIcon}></img>
           </a>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Header.css";
+import ProjectsIcon from "../images/Projects-icon.png";
+import SkillsIcon from "../images/Skills-icon.png";
+import BriefcaseIcon from "../images/Briefcase-icon.jpg";
+import PersonIcon from "../images/Person-icon.jpg";
+import CircleIcon from "../images/Circle-icon.jpg";
 
 export default function Header() {
   return (
     <div className="header">
       <h1>Mark Towey</h1>
       <div className="header-menu">
+        <img className="header-icon" src={ProjectsIcon}></img>
         <h2>Projects</h2>
+        <img className="header-icon" src={SkillsIcon}></img>
         <h2>Skills</h2>
+        <img className="header-icon" src={BriefcaseIcon}></img>
         <h2>Work</h2>
+        <img className="header-icon" src={PersonIcon}></img>
         <h2>Contact</h2>
+        <img className="header-icon" src={CircleIcon}></img>
         <h2>Dark/Light</h2>
       </div>
     </div>
