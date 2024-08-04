f7bf974372ce661eb9770dc4d37bbe90f077925b
Mark Towey <marktowey1@gmail.com>
2024-08-04 20:21:58 +0100

    Added functionality to the Download Resume button in the Contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,12 +4,33 @@ import "./Contact.css";
 import DownloadIcon from "../../images/Download-Icon.png";
 
 export default function Contact() {
+  function downloadResume() {
+    // Replace 'path/to/your/resume.pdf' with the actual path to your PDF file
+    const pdfUrl = "/MarkToweyCV.pdf";
+
+    // Create a temporary anchor element
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+
+    // Set the download attribute with the desired file name
+    link.download = "/MarkToweyCV.pdf";
+
+    // Append the link to the body
+    document.body.appendChild(link);
+
+    // Programmatically click the link to trigger the download
+    link.click();
+
+    // Remove the link from the body
+    document.body.removeChild(link);
+  }
+
   return (
     <div className="contact-container">
       <h1>Contact</h1>
       <p>Want to get in touch? Contact me on any of the following platforms:</p>
       <SocialLinks />
-      <a>
+      <a onClick={downloadResume}>
         <button className="download-btn">
           <img src={DownloadIcon} alt="Download Icon" />
           Download Resume
--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -8,16 +8,23 @@ import GmailIcon from "../../images/Gmail-icon.png";
 export default function SocialLinks() {
   return (
     <div className="social-links">
-      <a href="mailto:marktowey1@gmail.com">
+      <a
+        href="https://mail.google.com/mail/?view=cm&fs=1&to=marktowey1@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img className="socials-icon" src={GmailIcon}></img>
       </a>
-      <a href="https://www.linkedin.com/in/mark-towey-88231914a/">
+      <a
+        href="https://www.linkedin.com/in/mark-towey-88231914a/"
+        target="_blank"
+      >
         <img className="socials-icon" src={LinkedInIcon}></img>
       </a>
-      <a href="https://x.com/toweym3">
+      <a href="https://x.com/toweym3" target="_blank">
         <img className="socials-icon" src={TwitterIcon}></img>
       </a>
-      <a href="https://github.com/marktowey1">
+      <a href="https://github.com/marktowey1" target="_blank">
         <img className="socials-icon" src={GithubIcon}></img>
       </a>
     </div>
